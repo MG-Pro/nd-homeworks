5f1ad821207fdea7939f6d7f94ea4872111dbc80
MG <mg84@bk.ru>
2018-05-13 11:52:44 +0300

    hw1 Task#2 'read-all' in work

---
+++ modules-files/read-all/read-all.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+module.exports = (path) => {
+  return new Promise((done, fail) => {
+    fs.readdir(path, (err, list) => {
+      if (err) {
+        fail(err);
+      } else {
+        done(list);
+      }
+
+    })
+  })
+    .then(list => {
+      return list.map((item) => {
+        let a = {
+          name: item,
+          content: fs.readFile(`${path}${item}`, 'utf8', (err, data) => {
+            if (err)
+              console.log(err);
+            else
+              return data;
+          })
+        };
+        console.log(a);
+      })
+    })
+};
