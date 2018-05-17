7061a461f398af92cb8e247b9701941f7209a16c
mg84 <mg84@bk.ru>
2018-05-17 14:39:06 +0300

    hw2 Task#3-extra 'event' cmpl

--- eventemmiter/resourses/index.js
+++ eventemmiter/resourses/index.js
@@ -1,41 +1,16 @@
-const EventEmitter = require('events');
-
-class ChatApp extends EventEmitter {
-  /**
-   * @param {String} title
-   */
-  constructor(title) {
-    super();
-
-    this.title = title;
-
-    // Посылать каждую секунду сообщение
-    setInterval(() => {
-      this.emit('message', `${this.title}: ping-pong`);
-    }, 1000);
-  }
-
-  close() {
-    this.emit('close');
-  }
-}
-
-let webinarChat = new ChatApp('webinar');
-let facebookChat = new ChatApp('=========facebook');
-let vkChat = new ChatApp('---------vk');
+const facebookChat = require('./facebookChat');
+const vkChat = require('./vkChat');
+const webinarChat = require('./webinarChat');
 
 let chatOnMessage = (message) => {
   console.log(message);
 };
-
 const prepResponse = () => {
   console.log('Готовлюсь к ответу');
 };
 const closeHandler = () => {
   console.log('Чат вконтакте закрылся :(');
 };
-
-webinarChat.on('message', prepResponse);
 
 vkChat.setMaxListeners(2);
 
@@ -45,8 +20,6 @@ vkChat.on('message', prepResponse);
 webinarChat.on('message', chatOnMessage);
 facebookChat.on('message', chatOnMessage);
 vkChat.on('message', chatOnMessage);
-
-
 
 // Закрыть вконтакте
 setTimeout(() => {
---
+++ eventemmiter/resourses/vkChat.js
@@ -0,0 +1,3 @@
+const ChatApp = require('./ChatApp');
+
+module.exports = new ChatApp('---------vk');
