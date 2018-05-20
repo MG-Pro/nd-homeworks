33c1a4aab20bbfe64dc9ecb50f336d6c429fc486
mg84 <mg84@bk.ru>
2018-05-21 00:17:36 +0300

    hw3 Task#2 'stream' cmpl

--- stream/stream.js
+++ stream/stream.js
@@ -1,15 +1,30 @@
 const crypto = require('crypto');
 const fs = require('fs');
+const { Transform } = require('stream');
 
+class HexTransform extends Transform {
+  constructor(options) {
+    super(options);
+  }
 
-let filename = 'data.txt';
+  _transform(chunk, enc, cb) {
+    console.log(Buffer.from(chunk, 'hex'));
+    console.log(chunk);
+    let a = Buffer.from(chunk, 'hex').toString('hex');
+    this.push(a);
+    cb();
+  }
+}
 
-let res;
+const filename = 'data.txt';
+
+const hex = new  HexTransform();
 
 const input = fs.createReadStream(filename);
 const hash = crypto.createHash('md5');
-const output = fs.createWriteStream('hash.txt');
-res = input.pipe(hash);
 
-res.pipe(process.stdout);
-res.pipe(output);
+const output = fs.createWriteStream('hash.txt');
+const res = input.pipe(hash);
+res.pipe(hex);
+hex.pipe(process.stdout);
+hex.pipe(output);
