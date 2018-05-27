d3213a89b877b295dfea0a20e660e971538607eb
MG <mg84@bk.ru>
2018-05-27 14:36:26 +0300

    hw5 Task#1 'http' cmpl

--- http/http.js
+++ http/http.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const base = './public';
 
 function checkFile(filename) {
@@ -24,15 +25,41 @@ function checkFile(filename) {
 }
 
 function handler(req, res) {
-  checkFile(base + req.url)
-    .then(filename => {
-      res.writeHead(200, 'OK', {'Content-Type': 'text/html'});
-      fs.createReadStream(filename).pipe(res);
-    })
-    .catch(err => {
-      res.writeHead(404, http.STATUS_CODES[404], {'Content-Type': 'text/plain'});
-      res.end('File not found');
+  if (req.method === 'GET') {
+    checkFile(base + req.url)
+      .then(filename => {
+        res.writeHead(200, 'OK', {'Content-Type': 'text/html'});
+        fs.createReadStream(filename).pipe(res);
+      })
+      .catch(err => {
+        res.writeHead(404, http.STATUS_CODES[404], {'Content-Type': 'text/plain'});
+        res.end('File not found');
+      });
+  } else if (req.method === 'POST') {
+    function responseHandler(response) {
+      let data = '';
+      response.on('data', function (chunk) {
+        data += chunk;
+      });
+      response.on('end', function () {
+        res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+        res.write(data);
+        res.end();
+      });
+    }
+
+    let data = "";
+    req.on('data', function (chunk) {
+      data += chunk;
     });
+    req.on('end', function () {
+      const obj = JSON.parse(data);
+      const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160723T183155Z.f2a3339517e26a3c.d86d2dc91f2e374351379bb3fe371985273278df&text=${encodeURIComponent(obj.val)}&lang=ru-en`;
+      const request = https.request(url);
+      request.on('response', responseHandler);
+      request.end();
+    });
+  }
 }
 
 const server = http.createServer();
--- http/public/fetch.js
+++ http/public/fetch.js
@@ -2,21 +2,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('form');
 
-  form[1].addEventListener('click', (e) => {
+  form[2].addEventListener('click', (e) => {
     e.preventDefault();
+    const data = new FormData();
+    data.append('val', form[0].value);
     const url = window.location.origin;
     fetch(url, {
       method: 'post',
-      body: new FormData(form)
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        val: form[0].value,
+      })
     })
       .then((response) => {
-        return response.text();
+        return response.json();
       })
-      .then((body) => {
-        document.body.innerHTML = body;
+      .then((json) => {
+        form[1].value = json.text[0];
       })
-
-
   })
-
 });
