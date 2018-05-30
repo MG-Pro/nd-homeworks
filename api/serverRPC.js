791904a248695099d484ea9aefb1f7912d086482
MG <mg84@bk.ru>
2018-05-30 23:36:07 +0300

    hw6 Task#2 'rest' cmpl

--- api/serverRPC.js
+++ api/serverRPC.js
@@ -3,72 +3,117 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const users = [];
-const RPC = [
+const RPC = {
+  getAllUsers: (params, callback) => {
+    if (!params) {
+      return callback(new Error('Invalid params'));
+    }
+    try {
+      callback(null, users);
+    } catch (e) {
+      callback(e);
+    }
+  },
+  getUser: (params, callback) => {
+    if (!params) {
+      return callback(new Error('Invalid params'));
+    }
+    try {
+      const user = users[params.userId];
+      if (user) {
+        callback(null, user);
+      } else {
+        callback({
+          msg: `User ${params.userId} undefined`
+        });
+      }
 
-];
+    } catch (e) {
+      callback(e);
+    }
+  },
+  addUser: (params, callback) => {
+    if (!params) {
+      return callback(new Error('Invalid params'));
+    }
+    const newUserData = params.userData;
+    const id = users.length;
+    const result = {};
+    const badResult = {};
+    try {
+      Object.keys(newUserData).forEach((item) => {
+        if (item === 'name' || item === 'score') {
+          result[item] = newUserData[item];
+        } else {
+          badResult[item] = newUserData[item];
+        }
+      });
+
+      if (Object.keys(result)) {
+        users.push(result);
+        callback(null, {
+          id,
+          msg: 'OK',
+          valid: result,
+          invalid: badResult
+        });
+      } else {
+        callback({
+          msg: 'Invalid property name',
+          invalid: badResult
+        });
+      }
+    } catch (e) {
+      callback(e);
+    }
+  },
+  deleteUser: (params, callback) => {
+    if (!params) {
+      return callback(new Error('Invalid params'));
+    }
+    try {
+      users[params.userId] = null;
+      callback(null, {
+        msg: `User ${params.userId} deleted`
+      })
+    } catch (e) {
+      callback(e);
+    }
+  },
+  editUser: (params, callback) => {
+    if (!params) {
+      return callback(new Error('Invalid params'));
+    }
+    try {
+      let user = users[params.userId];
+      if (user) {
+        user = Object.assign(user, req.body);
+        users[params.userId] = user;
+        callback(null, user)
+      } else {
+        callback({
+          msg: `User ${params.userId} undefined`
+        });
+      }
+    } catch (e) {
+      callback(e);
+    }
+  }
+};
 
 app.use(bodyParser.json());
 
-app.get('/users/', (req, res) => {
-  res.json(users.filter(user => user));
-});
-
-app.post('/users/', (req, res) => {
-  const id = users.length;
-  const result = {};
-  const badResult = {};
-  Object.keys(req.body).forEach((item) => {
-    if (item === 'name' || item === 'score') {
-      result[item] = req.body[item];
+app.post('/', (req, res) => {
+  const method = RPC[req.body.method];
+  method(req.body.params, function (error, result) {
+    if (error) {
+      res.json(error);
     } else {
-      badResult[item] = req.body[item];
+      res.json(result);
     }
   });
-
-  if(Object.keys(result)) {
-    users.push(result);
-    res.json({
-      id,
-      status: 'OK',
-      valid: result,
-      invalid: badResult
-    });
-    res.send();
-  } else {
-    res.status(400);
-    res.json({
-      status: 'Invalid property name',
-      invalid: badResult
-    });
-    res.send();
-  }
-});
-
-app.get('/users/:userId', (req, res) => {
-  const user = users[req.params.userId];
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404);
-    res.send();
-  }
-});
-
-app.delete('/users/:userId', (req, res) => {
-  users[req.params.userId] = null;
-  res.send();
-});
-
-app.put('/users/:userId', (req, res) => {
-  let user = users[req.params.userId];
-  if (user) {
-    user = Object.assign(user, req.body);
-    users[req.params.userId] = user;
-    res.json(user);
-  } else {
-    res.status(404);
-    res.send();
-  }
 });
 
 app.listen(3000, () => console.log('App started on 3000 port'));
+
+//{"method": "getAllUsers"}
