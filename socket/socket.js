2f9542a7ea7959d811d7b1357ae770dda318cfb4
MG <mg84@bk.ru>
2018-06-05 23:38:37 +0300

    hw8 Task#1 'socket' in work

--- socket/socket.js
+++ socket/socket.js
@@ -2,24 +2,33 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const users ={};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-
-    io.emit('chat message', msg);
-
+  socket.on('chat message', function(data){
+    io.emit('chat message', data);
   });
 
-  const name = `User${(socket.id).toString().substr(1,2)}`;
-  socket.broadcast.emit('newUser', name);
+  const name = `User${Object.keys(users).length}`;
+  users[name] = name;
+  const data = {name: name};
 
-  socket.emit('userName', name);
+  socket.emit('initName', data);
+  socket.broadcast.emit('newUser', data);
+
+  socket.on('create', function(room) {
+    socket.join(room);
+    socket.emit('newRoom', {name: room});
+  });
+
+  socket.on('join', function(room) {
+    socket.join(room);
+    socket.emit('joinRoom', {name: room});
+  });
 });
 
 http.listen(3000, () => {
