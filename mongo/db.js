5e3659612ec483062a556580ba99872baf74a324
MG <mg84@bk.ru>
2018-06-09 00:11:07 +0300

    hw9 Task#1 'mongo' in work. add method add

--- mongo/db.js
+++ mongo/db.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
+const readline = require('readline');
+
 const url = 'mongodb://localhost:27017';
-const dbName = 'test';
 
 const users = [
   {name: 'Anya', age: 25},
@@ -8,27 +9,71 @@ const users = [
   {name: 'Ivan', age: 15}
 ];
 
+
+// add {"dbName": "test", "coll": "users", "doc": [{"name": "Anya", "age": "25"},{"name": "Lena", "age": "35"},{"name": "Ivan", "age": "15"}]}
+//
+//
+//
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.prompt();
+
 MongoClient.connect(url, function (err, client) {
   if (err) {
     console.log(new Error(err));
   }
-  console.log("Connected successfully to server");
-  
-  const db = client.db(dbName);
-  const collection = db.collection('users');
-  
-  collection.insert([users[0], users[0]], (err, res) => {
-    if (err) {
-      console.log(new Error(err));
+  console.log(`Connected successfully to server. Input add, del, up or exit to JSON`);
+
+  rl.on('line', (line) => {
+    const com = line.trim().split(' ', 1)[0];
+    const query = line.match(/\{(.+)\}/i)[0];
+    let data;
+
+    try {
+      data = JSON.parse(query);
+    } catch(e) {
+      console.log(e);
     }
-    console.log(res.ops);
-    client.close();
+    //console.log(data);
+
+    switch(com) {
+      case 'add':
+        const db = client.db(data.dbName);
+        const collection = db.collection(data.coll);
+        collection.insert(data.doc, function(err, res){
+          console.log(`Add data to db ${data.dbName} collection ${data.coll}: ${res.ops}`);
+          client.close();
+        });
+        break;
+      case 'find':
+        console.log('world!');
+        break;
+      case 'del':
+        console.log('world!');
+        break;
+      case 'up':
+        console.log('world!');
+        break;
+      case 'exit':
+        client.close();
+        rl.close();
+        break;
+      default:
+        console.log(`Input add, del, up or exit to JSON`);
+        break;
+    }
+    rl.prompt();
+  }).on('close', () => {
+    console.log('Close');
+    process.exit(0);
   });
-  
-  
-  
-  
-  //assert.equal(null, err);
-  
-  
+
 });
+
+
+
+
