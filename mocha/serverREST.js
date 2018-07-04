4ec779e226d77e77fbd56298e51d37a7b7d5bcf3
MG <mg84@bk.ru>
2018-07-04 21:16:15 +0300

    hw12 Task#1 'mocha' cmpl

--- mocha/serverREST.js
+++ mocha/serverREST.js
@@ -2,38 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const users = [
-  {name: 'vasa', score: 10},
-  {name: 'misha', score: 20},
-  {name: 'sasha', score: 17},
-  {name: 'masha', score: 15},
-  {name: 'dart', score: 100},
-  {name: 't1000', score: 1000}
-];
-
 app.use(bodyParser.json());
-
-app.get('/users/', (req, res) => {
-  let begin = +req.query.offset || 0;
-  let end = (begin + +req.query.limit) || users.length - 1;
-  console.log(begin, end);
-
-  let result = users.slice(begin, end);
-  console.log(req.query.fields);
-  if (req.query.fields) {
-    const fields = req.query.fields;
-
-    result = result.map((user, index) => {
-      const resultUser = {};
-      fields.forEach((field) => {
-        resultUser[field] = result[index][field];
-      });
-      return resultUser;
-    });
-  }
-
-  res.json(result);
-});
 
 app.post('/users/', (req, res) => {
   const id = users.length;
@@ -49,29 +18,16 @@ app.post('/users/', (req, res) => {
   if (Object.keys(result)) {
     users.push(result);
     res.json({
-      id,
+      id: id,
       status: 'OK',
       valid: result,
       invalid: badResult
     });
-    res.send();
   } else {
-    res.status(400);
     res.json({
-      status: 'Invalid property name',
+      status: 'Error',
       invalid: badResult
     });
-    res.send();
-  }
-});
-
-app.get('/users/:userId', (req, res) => {
-  const user = users[req.params.userId];
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404);
-    res.send();
   }
 });
 
@@ -80,25 +36,6 @@ app.delete('/users/:userId', (req, res) => {
   res.send();
 });
 
-app.put('/users/:userId', (req, res) => {
-  let user = users[req.params.userId];
-  if (user) {
-    user = Object.assign(user, req.body);
-    users[req.params.userId] = user;
-    res.json(user);
-  } else {
-    res.status(404);
-    res.send();
-  }
-});
-
-// delete all users
-
-app.delete('/users/', (req, res) => {
-  while (users.length > 0) {
-    users.pop();
-  }
-  res.send(users);
-});
-
 app.listen(3000, () => console.log('App started on 3000 port'));
+
+exports.app = app;
